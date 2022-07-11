a35e788de2dc47f57f32463d0b448b84d695605e
Saarthak Aggarwal <saggarwal29@wisc.edu>
2022-07-11 23:11:09 +0530

    Django files commit

---
+++ webApp/main/views.py
@@ -0,0 +1,12 @@
+from http.client import HTTPResponse
+from django.shortcuts import render
+from django.http import HttpResponse
+
+# Create your views here
+
+def index(request):
+    context = {
+        "name" : "Saarthak"
+    }
+
+    return render(request, "index.html", context)
