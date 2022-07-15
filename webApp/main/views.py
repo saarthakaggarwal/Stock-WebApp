99489780fe86aae22703224d056a90787290a327
Saarthak Aggarwal <saggarwal29@wisc.edu>
2022-07-16 02:15:12 +0530

    added top headline to the home page

--- webApp/main/views.py
+++ webApp/main/views.py
@@ -1,12 +1,33 @@
 from http.client import HTTPResponse
 from django.shortcuts import render
 from django.http import HttpResponse
+import requests
+import finnhub
 
 # Create your views here
 
 def index(request):
+
+    finnhub_client = finnhub.Client(api_key="cb8rlfaad3i0v9a1umlg")
+    newsData = finnhub_client.general_news('general', min_id=0)
+
+    first_news = newsData[0]
+    second_news = newsData[1]
+    third_news = newsData[2]
+
+    first_news_title = first_news["headline"]
+    second_news_title = second_news["headline"]
+    third_news_title = third_news["headline"]
+    
+
     context = {
-        "name" : "Saarthak"
+        "name" : "Saarthak",
+        "first_news_title" : first_news_title,
+        "second_news_title" : second_news_title,
+        "third_news_title" :  third_news_title,
+        "first_news_image" : first_news["image"],
+        "second_news_image" : second_news["image"],
+        "third_news_image" : third_news["image"]
     }
 
     return render(request, "index.html", context)
